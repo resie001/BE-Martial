4528f2cf535eaaad24a4191970ca9e6345e0f7a0
aderesie <aderesie.m.s@gmail.com>
2020-12-12 22:20:38 +0700

    Insial router hospital dengan alamat '/'

--- routes/hospitalRouter.js
+++ routes/hospitalRouter.js
@@ -5,8 +5,32 @@ var hospitalRouter = express.Router();
 
 hospitalRouter.use(bodyParser.json())
 
-hospitalRouter.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// Router Rumah Sakit dengan address '/'
+hospitalRouter.route('/')
+  .get((req, res) => {
+    Hospital.find({}).then((hospitals) => {
+      res.status = 201
+      res.setHeader('Content-Type', 'application/json')
+      res.json(hospitals)
+    }).catch((error) => {
+      res.status = error.statusCode
+      res.setHeader('Content-Type', 'application/json')
+      res.end = error.message.toString()
+    })
+  })
+  .post((req, res) => {
+    Hospital.create(req.body).then(() => {
+      res.status = 201
+      res.end('Berhasil menambahkan Rumah Sakit')
+    })
+  })
+  .put((req, res) => {
+    res.status = 403
+    res.end('Put operation is not supported')
+  })
+  .delete((req, res) => {
+    res.status = 403
+    res.end('Delete operation is not supported')
+  })
 
-module.exports = router;
+module.exports = hospitalRouter;
