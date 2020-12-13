0d4923d02c2ceb57ea55b92180b988f8ecd9a413
zikrullah79 <zikrullahaliwainda@gmail.com>
2020-12-14 02:07:18 +0800

    modify port

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var transactionRouter = require('./routes/TransactionRouter');
 var app = express();
 
 
-mongoose.connect('mongodb://localhost:27017/Martial', {
+mongoose.connect('mongodb+srv://martial:martial123@cluster0.qxzax.mongodb.net/martial?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
