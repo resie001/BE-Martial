b8b3e96bcc57134eed7dae8c2a43a36bc931549e
zikrullah79 <zikrullahaliwainda@gmail.com>
2020-12-14 23:42:51 +0800

    solve conflict

--- app.js
+++ app.js
@@ -4,16 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-const mongoose = require('mongoose');
+
+//Addition - Purusadi
+var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-
-var indexRouter = require('./routes/index');
-var recipeRouter = require('./routes/RecipeRouter');
-var transactionRouter = require('./routes/TransactionRouter');
-
-var app = express();
-
-
+//Koneksi ke DB
 mongoose.connect('mongodb+srv://martial:martial123@cluster0.qxzax.mongodb.net/martial?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -26,9 +21,41 @@ mongoose.connect('mongodb+srv://martial:martial123@cluster0.qxzax.mongodb.net/ma
         console.log('Error in DB connection: ' + err)
     }
 });
+
+var cookieSession = require('cookie-session');
+var passport = require('passport');
+var Drugs = require("./model/Drug");
+var passportSetup = require('./config/passport-setup');
+var keys = require('./config/keys');
+
+var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
+//Additional Router
+var drugRouter = require('./routes/drugRouter');
+var authRouter = require('./routes/authRouter');
+var profileRouter = require('./routes/profileRoutes');
+var userRouter = require('./routes/userRouter');
+var recipeRouter = require('./routes/RecipeRouter');
+var transactionRouter = require('./routes/TransactionRouter');
+
+
+
+var app = express();
+
+// set up session cookies
+app.use(cookieSession({
+  maxAge: 24 * 60 * 60 * 1000,
+  keys: [keys.session.cookieKey]
+}));
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -37,8 +64,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+//app.use('/users', usersRouter);
+//Additional app.use
+app.use('/drug', drugRouter);
+app.use('/auth', authRouter);
+app.use('/profile', profileRouter);
+app.use('/users', userRouter);
 app.use('/recipe', recipeRouter);
 app.use('/transaction', transactionRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- model/User.js
+++ model/User.js
@@ -1,20 +1,27 @@
 const mongoose = require('mongoose'); 
 
 var userSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true,
-        index:true,
-    },
-    password:{
-        type:String,
-        required:true,
-    },
-    role:{
-        type:String,
-        required:true,
-    },
+    name: {
+        type: String,
+        required: true
+      },
+      email: {
+        type: String,
+        required: true
+      },
+      role: {
+        type: String,
+        default: "user",
+        enum: ["user", "dokter", "superadmin"]
+      },
+      username: {
+        type: String,
+        required: true
+      },
+      password: {
+        type: String,
+        required: true
+      },
     data_id:{
         type:mongoose.Schema.Types.ObjectId,
         required:true,
