30e8198051e2db02eec27f6e35673026a03deef2
aderesie <aderesie.m.s@gmail.com>
2020-12-16 04:03:28 +0700

    Router login

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -5,16 +5,19 @@ var authRouter = express.Router();
 
 authRouter.route('/login')
     .get((req, res) => {
-        res.status = 403
+        res.status(403)
         res.end('Get operation is not supported')
     })
     .post((req, res) => {
         if (req.body.username === 'admin' && req.body.password === 'admin') {
             var token = jwt.sign({ username: req.body.username, password: req.body.password }, "UK1SbazgQ3yynO3Mg2bgaONSuD5rM0CqIccoUhWqk3NgUqDqQ3GBYCWxZkKsV36z")
-            if(token){
-
+            if (token) {
+                res.status(201)
+                res.setHeader('Content-Type', 'application/json')
+                res.json(token)
             } else {
-
+                res.status(201)
+                res.end('Login gagal')
             }
         } else {
             res.status(404)
@@ -22,11 +25,11 @@ authRouter.route('/login')
         }
     })
     .put((req, res) => {
-        res.status = 403
+        res.status(403)
         res.end('Update operation is not supported')
     })
     .delete((req, res) => {
-        res.status = 403
+        res.status(403)
         res.end('Delete operation is not supported')
     })
 
