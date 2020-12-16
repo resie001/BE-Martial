a784934e1c6aa5f3fad8281673dd4f814f1cd35f
zikrullah79 <zikrullahaliwainda@gmail.com>
2020-12-17 01:44:42 +0800

    check

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 //Koneksi ke DB
 
-mongoose.connect(process.env.TEST_URI, {
+mongoose.connect(process.env.BASE_URI+"&"+process.env.URI_OPTION, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
